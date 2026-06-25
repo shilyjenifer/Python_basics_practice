730cd136ee03e18ad5bf5336c9ba94a7e9628423
shilyjenifer <shilyjenifer@gmail.com>
2026-06-25 15:38:31 +0530

    Initial Commit

---
+++ practice1.py
@@ -0,0 +1,11 @@
+cust_name = input("Enter your name :")
+
+product_price = float(input("Product_price: "))
+
+quantity = int(input("Quantity purchased: "))
+
+revenue = product_price * quantity
+
+print( "\nCustomer :",cust_name)
+
+print("\nRevenue: Rs.",revenue,"/-")
